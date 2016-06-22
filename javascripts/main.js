00f973dfcd49847c5db685c13f0c38e7b03b1db9
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-06-22 11:07:38 -0500

    changes types json(s) to one json

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,18 @@
 "use strict";
 var mainJS = (function(){
 	$(document).ready(function(){
-		
+		// event listeners
+		$("#category").on("change", "#fireworks", getCategory);
+
+		var getCategory = function(){
+			return new Promise((resovle, reject) => {
+				$.ajax({
+	      	url: "../json/categories.json"
+	    	}).done(function(data) {
+	      	resolve(data);
+	    	}).fail(function(xhr, status, error) {
+	      	reject(error);
+			});
+		};
 	});
 });
