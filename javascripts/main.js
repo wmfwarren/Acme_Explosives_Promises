99ba024d52d3dd553173f1003a5b1e59d896974b
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-06-22 14:18:03 -0500

    types load now

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,25 +2,85 @@
 
 $(document).ready(function(){
 	var mainJS = (function(){
+		var categoryName = null;
+		var typeName = null;
 		var categoryID = null;
-		// event listeners
-		console.log("I load");
-		$(document).on("change", "#category",function(event){
+		var typeID = null;
+		// event listener
+		$(document).on("change", "#category", function(event){
 			getCategory(event)
 			.then(function(category){
 				return applyTypes(category);
+			}).then(function(types){
+	      return changeTypeSelectors(types);
 			});
 		});
+
+		$(document).on("change", "#type", function(event){
+			loadProducts(event)
+			.then(function(types){
+				return resolveType(types);
+			});
+		});	
 	//helper function for get category
 		function resolveCategory(data){
-			for (let i = 0; i < data.category.length; i++){
-				var targetCategory = data.category[i];
-				if ($("#category").value() === targetCategory.name){
-					var categoryName = targetCategory.name;
+			for (let i = 0; i < data.categories.length; i++){
+				var targetCategory = data.categories[i];
+				if ($("#category").val() === targetCategory.name){
+					categoryName = targetCategory.name;
 					categoryID = targetCategory.id;
 				}
 			}
-			return categoryID;
+			categoryName = categoryName;
+			categoryID = categoryID;
+		}
+	//helper
+		function resolveType(data) {
+			for (let i = 0; i < data.types.length; i++){
+				var targetType = data.types[i];
+				if ($("#type").val() === targetType.name){
+					typeName = targetType.name;
+					typeID = targetType.id;
+				}
+			}
+			typeName = typeName;
+			typeID = typeID;
+		}
+	//helper for the resolveCategory helper
+		function changeTypeSelectors( JSONObject){
+			var $typesZone = $("#type");
+			var categoryType = JSONObject.types[categoryID];
+			
+			$typesZone.children().remove(); //remove old children
+
+			$typesZone.append(`<option id="none">-</option>`);
+
+			for (let i = 0; i < categoryType.length; i++){
+				$typesZone.append(`<option id="${categoryType[i].name}">${categoryType[i].name}</option>`);
+			}
+			
+		}
+	//helper
+		// function setTypeID(typeToMatch){
+		// 	for (let i = 0; i < typeToMatch.length; i++){
+		// 		if ($("#type").val() === categoryToMatch.name){
+		// 			typeID = categoryToMatch[i].id;
+		// 		}
+		// 	}
+		// }
+	//helper for adding to the product dropdown
+		function changeProductSelectors(JSONTypeObject){
+			var $productZone = $("#product");
+			var typeProduct = JSONTypeObject.products[typeID];
+
+			$productZone.children().remove(); //remove old children
+
+			$productZone.append(`<option id="none">-</option>`);
+			for (let keys in typeProduct){
+					$productZone.append(`<option id="${typeProduct[keys]}">${typeProduct[keys]}</option>`);
+			}
+
+
 		}
 	//gets the category
 		var getCategory = function(event){
@@ -28,26 +88,41 @@ $(document).ready(function(){
 				$.ajax({
 	      	url: "../json/categories.json"
 	    	}).done(function(data) {
-	      	resovle(data);
-	      	//console.log(data);
+	      	 resolveCategory(data);
+	      	resovle(data );
 	    	}).fail(function(xhr, status, error) {
 	      	reject(error);
 				});
 			});
 		};
 
-		var applyTypes = function(category) {
+		var applyTypes = function(category, categoryID) {
 	  return new Promise((resolve, reject) => {
 	    $.ajax({
 	      url: "../json/types/types.json",
-	      data: category
+	      // data: category
 	    }).done(function(data) {
 	      resolve(data);
-	      console.log(category);
 	    }).fail(function(xhr, status, error) {
 	      reject(error);
 	    });
 		  }
 		)};
+
+	  var loadProducts = function (){
+			return new Promise((resolve, reject) => {
+		    $.ajax({
+		      url: `../json/types/${categoryName}Products.json`,
+		      // data: category
+		    }).done(function(data) {
+		    	changeProductSelectors(data);
+		      resolve(data);
+		      console.log(data);
+		    }).fail(function(xhr, status, error) {
+		      reject(error);
+		    });
+			  }
+		)};
+
 	})();
 });
