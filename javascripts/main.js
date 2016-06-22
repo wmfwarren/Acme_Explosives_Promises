8f8ef21d83ac68a312a1536adb920d361a7c0ca9
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-06-22 12:27:07 -0500

    Fixed Json parse error

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,18 +1,53 @@
 "use strict";
-var mainJS = (function(){
-	$(document).ready(function(){
-		// event listeners
-		$("#category").on("change", "#fireworks", getCategory);
 
-		var getCategory = function(){
+$(document).ready(function(){
+	var mainJS = (function(){
+		var categoryID = null;
+		// event listeners
+		console.log("I load");
+		$(document).on("change", "#category",function(event){
+			getCategory(event)
+			.then(function(category){
+				return applyTypes(category);
+			});
+		});
+	//helper function for get category
+		function resolveCategory(data){
+			for (let i = 0; i < data.category.length; i++){
+				var targetCategory = data.category[i];
+				if ($("#category").value() === targetCategory.name){
+					var categoryName = targetCategory.name;
+					categoryID = targetCategory.id;
+				}
+			}
+			return categoryID;
+		}
+	//gets the category
+		var getCategory = function(event){
 			return new Promise((resovle, reject) => {
 				$.ajax({
 	      	url: "../json/categories.json"
 	    	}).done(function(data) {
-	      	resolve(data);
+	      	resovle(data);
+	      	//console.log(data);
 	    	}).fail(function(xhr, status, error) {
 	      	reject(error);
+				});
 			});
 		};
-	});
+
+		var applyTypes = function(category) {
+	  return new Promise((resolve, reject) => {
+	    $.ajax({
+	      url: "../json/types/types.json",
+	      data: category
+	    }).done(function(data) {
+	      resolve(data);
+	      console.log(category);
+	    }).fail(function(xhr, status, error) {
+	      reject(error);
+	    });
+		  }
+		)};
+	})();
 });
